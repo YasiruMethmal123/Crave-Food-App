8752ba289e6a9fa5d9d80d7b095f1f2de3b9a0e1
YasiruMethmal123 <yasiru.methmal@outlook.com>
2025-07-27 22:46:42 +0530

    home page design

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,45 @@
-import { FlatList, Text, View } from "react-native";
-import "./global.css";
+import { images, offers } from "@/constants";
+import cn from 'clsx';
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
- 
+import "./global.css";
+
 export default function Index() {
   return (
-    <SafeAreaView>
-      <FlatList data={[offers]} renderItem={({item}) =>(<div/>)}/>
+    <SafeAreaView className="flex-1 bg-white">
+          <FlatList
+              data={offers}
+              renderItem={({ item, index }) => {
+                  const isEven = index % 2 === 0;
+
+                  return (
+                      <View>
+                          <Pressable className="offer-card" style = {{backgroundColor : item.color}}>
+                          {({ pressed }) => (
+                                  <Fragment>
+                                      <View className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
+                                      style={{ backgroundColor: item.color }}
+                                      >
+                                        <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
+                                      </View>
+                                      <View className={cn("offer-card__info" , isEven ? 'pl-10' : 'pr-10')}>
+                                        <Text className="h1-bold text-white leading-tight">
+                                              {item.title}
+                                        </Text>
+                                        <Image
+                                            source={images.arrowRight}
+                                            className="size-10"
+                                            resizeMode="contain"
+                                            tintColor="#ffffff"
+                                          />
+                                      </View>
+                                  </Fragment>
+                          )}
+                        </Pressable>
+                      </View>
+        )
+      }}/>
     </SafeAreaView>
   );
 }
