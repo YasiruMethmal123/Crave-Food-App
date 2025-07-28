fef4bd11f4b2bf3570889e2b08bbb8acec3182ea
YasiruMethmal123 <yasiru.methmal@outlook.com>
2025-07-28 22:02:55 +0530

    completed home page

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ import "./global.css";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <SafeAreaView>
           <FlatList
               data={offers}
               renderItem={({ item, index }) => {
@@ -15,19 +15,22 @@ export default function Index() {
 
                   return (
                       <View>
-                          <Pressable className="offer-card" style = {{backgroundColor : item.color}}>
-                          {({ pressed }) => (
+                          <Pressable
+                              className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
+                              style={{ backgroundColor: item.color }}
+                              android_ripple={{ color: "#fffff22"}}
+                          >
+                              {({ pressed }) => (
                                   <Fragment>
-                                      <View className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
-                                      style={{ backgroundColor: item.color }}
-                                      >
+                                      <View className={"h-full w-1/2"}>
                                         <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
                                       </View>
-                                      <View className={cn("offer-card__info" , isEven ? 'pl-10' : 'pr-10')}>
-                                        <Text className="h1-bold text-white leading-tight">
+
+                                      <View className={cn("offer-card__info", isEven ? 'pl-10': 'pr-10')}>
+                                          <Text className="h1-bold text-white leading-tight">
                                               {item.title}
-                                        </Text>
-                                        <Image
+                                          </Text>
+                                          <Image
                                             source={images.arrowRight}
                                             className="size-10"
                                             resizeMode="contain"
@@ -35,11 +38,12 @@ export default function Index() {
                                           />
                                       </View>
                                   </Fragment>
-                          )}
-                        </Pressable>
+                              )}
+                          </Pressable>
                       </View>
-        )
-      }}/>
+                  )
+              }}
+      />
     </SafeAreaView>
   );
 }
